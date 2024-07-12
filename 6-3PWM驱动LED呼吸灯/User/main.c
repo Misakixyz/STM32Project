16891840c5d4f2a754b74520a00b921b368dc830
Misaki <1099249596@qq.com>
2024-07-12 22:51:11 +0800

    6-3PWM呼吸灯完成

--- 6-3PWM驱动LED呼吸灯/User/main.c
+++ 6-3PWM驱动LED呼吸灯/User/main.c
@@ -1,21 +1,20 @@
 #include "stm32f10x.h" // Device header
-#include "Delay.h"
+// #include "Delay.h"
 #include "OLED.h"
-#include "Timer.h"
+// #include "Timer.h"
+#include "PWM.h"
 
 uint16_t NUM;
 int main(void)
 {
 	OLED_Init();
-	Timer_Init();
+	PWM_Init();
 
-	OLED_ShowString(1, 1, "NUM:");
-	OLED_ShowString(2, 1, "CNT:");
+
 
 
 	while (1)
 	{
-		OLED_ShowNum(1, 5, NUM, 5);
-		OLED_ShowNum(2, 5, Timer_GetCounter(), 5);
+
 	}
 }
